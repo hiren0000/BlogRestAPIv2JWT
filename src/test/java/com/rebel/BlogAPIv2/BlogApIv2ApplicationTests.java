5c85cc08bec3c985d5d0bfe5fcbd0b08ab09b7b2
Hiren <rebeldevmurari@gmail.com>
2023-01-16 22:09:45 +0000

    Initial Commit

---
+++ src/test/java/com/rebel/BlogAPIv2/BlogApIv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.BlogAPIv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApIv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
