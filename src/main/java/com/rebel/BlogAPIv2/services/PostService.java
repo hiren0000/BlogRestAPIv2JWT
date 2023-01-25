4a56d9a8f72c4273c63db3455b832a8ad6ac79eb
Hiren <rebeldevmurari@gmail.com>
2023-01-25 22:55:34 +0000

    Implemented PostServiceImpl, all the required service layer's functions are ready to test.

--- src/main/java/com/rebel/BlogAPIv2/services/PostService.java
+++ src/main/java/com/rebel/BlogAPIv2/services/PostService.java
@@ -17,19 +17,19 @@ public interface PostService
     void deletePost(Integer poId);
 
     //get all posts
-    List<Post> getAllPosts();
+    List<PostDto> getAllPosts();
 
     //get single post by id
     PostDto getPostById(Integer poId);
 
     //get all posts by userId
-    List<Post> getPostByUser(Integer userId);
+    List<PostDto> getPostByUser(Integer userId);
 
     //get all posts by categoryID
-    List<Post> getPostByCategory(Integer categoryId);
+    List<PostDto> getPostByCategory(Integer categoryId);
 
     //get post by searching
-    List<Post> searchPost(String keyword);
+    List<PostDto> searchPost(String keyword);
 
 
 
