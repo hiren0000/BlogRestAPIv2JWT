acfbd8b8277741028797a890d1e8abcf578b0ffe
Hiren <rebeldevmurari@gmail.com>
2023-01-31 22:24:35 +0000

    Adding comment entity and repo

---
+++ src/main/java/com/rebel/BlogAPIv2/payloads/CommentDto.java
@@ -0,0 +1,16 @@
+package com.rebel.BlogAPIv2.payloads;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentDto
+{
+    private Integer coId;
+
+    private String content;
+
+
+
+}
---
+++ src/main/java/com/rebel/BlogAPIv2/repo/CommentRepo.java
@@ -0,0 +1,11 @@
+package com.rebel.BlogAPIv2.repo;
+
+import com.rebel.BlogAPIv2.enitities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepo extends JpaRepository<Comment, Integer>
+{
+
+}
