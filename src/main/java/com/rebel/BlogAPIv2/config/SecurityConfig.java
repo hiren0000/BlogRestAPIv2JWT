181ee1419396e57a1c1ae502d88dfd790dc42231
Hiren <rebeldevmurari@gmail.com>
2023-02-09 22:43:51 +0000

    trying to implement the basic authorization through the db directly

--- src/main/java/com/rebel/BlogAPIv2/config/SecurityConfig.java
+++ src/main/java/com/rebel/BlogAPIv2/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.rebel.BlogAPIv2.config;
 
+import com.rebel.BlogAPIv2.security.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -20,5 +29,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .and()
                 .httpBasic();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(this.customUserDetailService).passwordEncoder(getEncode());
+
+    }
+
+    @Bean
+    public PasswordEncoder getEncode()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
